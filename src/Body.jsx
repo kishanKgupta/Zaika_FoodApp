c79ffcde5a44495794e3182710150a80c7cd88ff
kishan gupta <kkgupta0106@gmail.com>
2024-07-19 20:11:57 +0530

    Fourth Commit

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,7 +3,16 @@ const Card = () => {
     return (
         <>
             <div className="card">
-                <p>Meghna food</p>
+                <div className="res-logo">
+                    <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/b14cd9fc40129fcfb97aa7e621719d07" alt="food"/>
+                </div>
+                <div className="card-info">
+                    <h3>Meghna food</h3>
+                    <h4>Biryani,Sweets,North Indian</h4>
+                    <h4>4.4 stars</h4>
+                    <h4>30 minutes</h4>
+                </div>
+                
             </div>
         </>
     )
